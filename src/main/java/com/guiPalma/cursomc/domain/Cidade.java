5a21f3673903794219cfcf06e087cf10ac514a38
Guilherme Palma <gomes.lmc@gmail.com>
2017-10-14 13:39:18 -0300

    Cidade, Estado e banco H2

--- src/main/java/com/guiPalma/cursomc/domain/Cidade.java
+++ src/main/java/com/guiPalma/cursomc/domain/Cidade.java
@@ -8,34 +8,35 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Categoria implements Serializable{
+public class Cidade implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-	
-	public Categoria(Integer id, String nome) {
+		
+	public Cidade(Integer id, String nome) {
 		super();
 		this.id = id;
 		this.nome = nome;
 	}
+
+		
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -43,6 +44,8 @@ public class Categoria implements Serializable{
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -51,7 +54,7 @@ public class Categoria implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Cidade other = (Cidade) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -59,6 +62,5 @@ public class Categoria implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
+
 }
